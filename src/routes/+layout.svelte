266346148381ea5fbb81cc788d8eda0ee85d5b9a
Tim Rekelj <tim.rekelj@proton.me>
2023-08-10 12:21:20 +0200

    something

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let length = 12;
+    const small_letters = "abcdefghijkmnprstuvz"
+    const big_letters = "ABCDEFGJKLMNPRSTUVZ"
+    const numbers = "123456789"
+    const special_characters = "?!*=%#"
+
+    const chars = small_letters + big_letters + numbers + special_characters
+
+    let password = '';
+
+    onMount(async () => {
+
+        for (let i = 0; i < length; i++) {
+            password += chars[Math.floor(Math.random() * chars.length)];
+        }
+    });
+</script>
+
+<h1>Your password:</h1>
+<div>{password}</div>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,8 @@
 <script>
-    let length = 10;
-    function generate() {
-        const small_letters = "abcdefghijkmnprstuvz"
-        const big_letters = "ABCDEFGJKLMNPRSTUVZ"
-        const numbers = "123456789"
-        const special_characters = "?!*=%#"
-
-        const chars = small_letters + big_letters + numbers + special_characters
-
-        let password = '';
-
-        for (let i = 0; i < length; i++) {
-            password += chars[Math.floor(Math.random() * chars.length)];
-        }
-        alert(password);
-    }
+    import "../app.css";
 </script>
-
-<h1>Password manager</h1>
-
-<label>Password length:</label>
-
-<input type="number" min="1" max="100" bind:value={length} />
-
-<button on:click={generate}>Generate</button>
+  
+<svelte:head>
+	<title>Password Generator</title>
+	<meta name="Password Generator" />
+</svelte:head>
